cd14633a9ccf39eb2352404bfb71223353e2fa1a
Giovanny Putri Andini <86881191+giovannyptr@users.noreply.github.com>
2025-10-05 10:58:55 +0700

    added navbar and authentication

---
+++ trello-app-gio/app/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "@/components/navbar"
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-pink-200 via-white to-purple-200">
+    <Navbar />
+    </div>
+  )
+}
