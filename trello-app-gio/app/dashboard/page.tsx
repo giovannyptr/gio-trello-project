a6611e2c9e85b8efeb4f8156585acc944dfd7382
Giovanny Putri Andini <86881191+giovannyptr@users.noreply.github.com>
2025-10-05 21:16:34 +0700

    setup supabase

--- trello-app-gio/app/dashboard/page.tsx
+++ trello-app-gio/app/dashboard/page.tsx
@@ -2,25 +2,38 @@
 
 import Navbar from "@/components/navbar"
 import { useUser } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { useBoards } from "@/lib/hooks/useBoards";
 
 export default function DashboardPage() {
-const {user} = useUser()
+  const { user } = useUser()
+  const {createBoard} = useBoards()
+
+
+
+  const handleCreateBoard = async () => {
+    await createBoard({title: "New Board"});
+  }
 
   return (
-  <div className="min-h-screen bg-pink-50">
-    <Navbar/>
+    <div className="min-h-screen bg-pink-50">
+      <Navbar />
 
-    <main className="container mx-auto px-4 py-6 sm:py-8">
-      <div className="mb-6 sm:mb-8">
-        <h1 className="text-2xl sm:text-3xl font-bold text-grey-900 mb-2">
-          Welcome back, {user?.firstName ?? user?.emailAddresses[0].emailAddress}! 👋
-        </h1>
-        <p className="text-grey-600">
-          Here's what's happening with your boards today.
-        </p>
-      </div>
-
-    </main>
-  </div>
+      <main className="container mx-auto px-4 py-6 sm:py-8">
+        <div className="mb-6 sm:mb-8">
+          <h1 className="text-2xl sm:text-3xl font-bold text-grey-900 mb-2">
+            Welcome back, {user?.firstName ?? user?.emailAddresses[0].emailAddress}! 👋
+          </h1>
+          <p className="text-grey-600">
+            Here's what's happening with your boards today.
+          </p>
+          <Button className="w-full sm:w-auto" onClick={handleCreateBoard}>
+            <Plus className="h-4 w-4 mr-2" />
+            Create Board
+          </Button>
+        </div>
+      </main>
+    </div>
   )
 }
